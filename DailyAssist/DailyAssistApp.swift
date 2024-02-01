7d5d1094ba74db4a7dbcddd385a0c39288ec457b
Jack <jack@codered.gg>
2024-01-31 20:53:36 -0600

    Initial Commit

---
+++ DailyAssist/DailyAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAssistApp.swift
+//  DailyAssist
+//
+//  Created by Jack Coon on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
