983e52e392ed9674788e1fc94fa5458f56a732ec
Jack <jack@codered.gg>
2024-02-01 01:33:01 -0600

    Messing w/ Swift

---
+++ DailyAssist/ViewModels/NewsItem.swift
@@ -0,0 +1,8 @@
+//
+//  NewsItem.swift
+//  DailyAssist
+//
+//  Created by Jack Coon on 1/31/24.
+//
+
+import Foundation
---
+++ DailyAssist/Views/TodaysNewsView.swift
@@ -0,0 +1,8 @@
+//
+//  TodaysNewsView.swift
+//  DailyAssist
+//
+//  Created by Jack Coon on 1/31/24.
+//
+
+import Foundation
